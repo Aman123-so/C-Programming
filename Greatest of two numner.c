08dff9d6bb40eb9b92a1b936d281bbe878b9ea85
Aman Gupta <ammugupta893@gmail.com>
2025-01-26 20:41:31 +0530

    Add files via upload

---
+++ Greatest of two numner.c
@@ -0,0 +1,18 @@
+// write a program greatest of two number.
+#include <stdio.h>
+void main()
+{
+    int a, b;
+	printf("Enter First: ");
+	scanf("%d", &a);
+	printf("Enter Second: ");
+	scanf("%d", &b);
+	if (a > b)
+	{
+     printf("a is greater than b");
+	}
+	else
+	{
+		printf("a is lesser than b");
+	}
+}
