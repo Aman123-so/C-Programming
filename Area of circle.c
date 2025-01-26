13bba12b2b13eb88606a524d809ba5c9b3debcde
Aman Gupta <ammugupta893@gmail.com>
2025-01-26 19:56:16 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,10 @@
+// Area of circle
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("Enter value of radius: ");
+    scanf("%f", &radius);
+    printf(" Area of circle is : %f",3.14*radius*radius);
+    return 0;
+
+}
