5f9b50ba09706b2bc6bb426f157d92841238fe72
Aman Gupta <ammugupta893@gmail.com>
2025-01-26 20:19:38 +0530

    Add files via upload

---
+++ Check Even number.c
@@ -0,0 +1,9 @@
+// Check Even number
+// if even number print 1, else print 0
+#include<stdio.h>
+int main(){
+    int i;
+    printf("Enter a number: ");
+    scanf("%d", &i );
+    printf(" %d",i%2 == 0);
+}
